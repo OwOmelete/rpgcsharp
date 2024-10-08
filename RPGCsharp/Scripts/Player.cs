79c7a902a68c02fe569d79e2b64be1a9bb9768a6
OwOmelete <nathan.levylequart@gmail.com>
2024-10-08 11:36:02 +0200

    plan V3

--- RPGCsharp/Scripts/Player.cs
+++ RPGCsharp/Scripts/Player.cs
@@ -2,6 +2,12 @@ namespace RPGCsharp.Properties
 {
     public class Player
     {
+        public int Hp;
+        void CreatePlayer(int hp)
+        {
+            int[] player = {hp,};
+        }
+        
         //créer le player
         //attaque
         //inventaire
