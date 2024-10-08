1672a50b72b10176786e6578ceed37a63d662bea
Mrcookie <hautcoeur.corentin@gmail.com>
2024-10-08 12:53:42 +0200

    Ennemie + BadGuys plan

--- RPGCsharp/Scripts/Ennemi.cs
+++ RPGCsharp/Scripts/Ennemi.cs
@@ -1,10 +1,38 @@
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography;
+
 namespace RPGCsharp.Properties
 {
     public class Ennemi
-    {
-        //créer
-        //attaquer
-        //mourir
-        //loot
+    {/*
+        // créer
+        if(//hero goes in a room)
+        {
+            //random number of ennemie and random difficultie spawn
+        }
+        
+        // attaquer
+        if(mob turn = true)
+        {
+            // heroHP -= mobATK
+        }
+        // mourrir
+        if(// sword damage >= mob Hp)
+        {
+            // mob die
+        }
+        else
+        {
+            // mobHP -= sword damage;
+        }    
+        
+        // loot
+        if (// mob die)
+        {
+            // drop gold coin between 1 to 10 
+            // drop ???
+        }
+     */
     }
 }
